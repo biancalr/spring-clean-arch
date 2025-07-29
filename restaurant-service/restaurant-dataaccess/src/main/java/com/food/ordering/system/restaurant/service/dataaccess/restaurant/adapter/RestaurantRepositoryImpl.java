cb279dcc282bbcddd3c0832b4f1696668066cf19
biancalr <biancalr@github.com>
2025-07-28 21:11:03 -0300

    Restaurant Service - Implementing Data Access module

--- restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -1,10 +1,10 @@
-package com.food.ordering.system.order.service.dataaccess.restaurant.adapter;
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
 
 import com.food.ordering.system.data.access.restaurant.entity.RestaurantEntity;
 import com.food.ordering.system.data.access.restaurant.repository.RestaurantJpaRepository;
-import com.food.ordering.system.order.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
-import com.food.ordering.system.order.service.domain.entity.Restaurant;
-import com.food.ordering.system.order.service.domain.port.output.repository.RestaurantRepository;
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -24,11 +24,12 @@ public class RestaurantRepositoryImpl implements RestaurantRepository {
     }
 
     @Override
-    public Optional<Restaurant> findRestaurantinformation(Restaurant restaurant) {
-        List<UUID> restaurantProdcts =
+    public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
+        List<UUID> restaurantProducts =
                 restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
         Optional<List<RestaurantEntity>> restaurantEntities = restaurantJpaRepository
-                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(), restaurantProdcts);
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(),
+                        restaurantProducts);
         return restaurantEntities.map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
     }
 }
